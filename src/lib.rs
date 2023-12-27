f889a64bb0349f746e64486c0be457df1c8a373a
jsondevers <jasondevers@js-m1.lan>
2023-12-26 20:48:49 -0500

    latest

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@ pub mod tracker {
     pub mod http;
     pub mod udp;
 }
+/// Peer module contains all the logic for the peer to peer connection
+pub mod peer {
+    pub mod bitfield;
+    pub mod connection;
+    pub mod handshake;
+    pub mod manager;
+    pub mod message;
+}
+pub mod storage;
 pub mod utils;
 
 pub const DEBIAN_FILE: &str = "sample/debian.torrent"; // debian.torrent test torrent
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use crate::torrent::Torrent;
+use crate::tracker::http::HttpTracker;
+
 use rand::Rng;
 
 pub fn generate_peer_id() -> [u8; 20] {
@@ -5,4 +8,27 @@ pub fn generate_peer_id() -> [u8; 20] {
     let mut rng = rand::thread_rng();
     rng.fill(&mut peer_id);
     peer_id
+}
+
+pub fn get_peers(torrent_file: &str) -> Result<Vec<std::net::SocketAddr>, anyhow::Error> {
+    let torrent = Torrent::from_file(torrent_file).unwrap();
+    let peer_id = generate_peer_id();
+    let port = 6969;
+    let compact = Some(1);
+
+    let mut client = HttpTracker::new().unwrap();
+
+    let response = client.announce(&torrent, peer_id, port, compact).unwrap();
+
+    let peers = response.peers();
+
+    log::debug!("Peers: {:?}", peers);
+
+    Ok(peers)
+}
+
+pub fn get_pieces(torrent_file: &str) -> Result<Vec<[u8; 20]>, anyhow::Error> {
+    let torrent = Torrent::from_file(torrent_file).unwrap();
+    let pieces = torrent.piece_hashes();
+    Ok(pieces)
 }
